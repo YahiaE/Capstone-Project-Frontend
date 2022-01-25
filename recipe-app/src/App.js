e0a6ab1ec263afe823ca26f5aba022792a181c0f
DannieC97 <danielchabi97@gmail.com>
2022-01-25 15:49:23 -0500

    added allRecipes page and recipecards component, closes #9, closes #3

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import AllRecipes from './components/AllRecipes';
+import CreateRecipes from './components/CreateRecipes';
 
 function App() {
   return (
     <Router>
-      {/* <Navbar /> */}
       <div className="App">
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/allrecipes" element={<AllRecipes />} />
+          <Route exact path="/createrecipes" element={<CreateRecipes />} />
         </Routes>
       </div>
       </Router>
---
+++ recipe-app/src/components/AllRecipes.js
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar"
+import RecipeCards from "./RecipeCards"
+export default function AllRecipes(){
+    return(
+        <div>
+            <Navbar />
+            <RecipeCards />
+        </div>
+    )
+}
